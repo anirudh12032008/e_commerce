53603a2da72963268227439ab2fc88348867f994
Anirudh Sahu <renudilip19@gmail.com>
2023-07-22 12:59:27 +0530

    added add-products

--- src/app/add-product/page.js
+++ src/app/add-product/page.js
@@ -35,44 +35,49 @@ async function addProduct(formData) {
 
 export default async function AddProductPage() {
   const session = await getServerSession(authOptions);
-
   if (!session) {
     redirect("/api/auth/signin?callbackUrl=/add-product");
   }
 
-  return (
-    <div>
-      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
-      <form action={addProduct}>
-        <input
-          required
-          name="name"
-          placeholder="Name"
-          className="input-bordered input mb-3 w-full"
-        />
-        <textarea
-          required
-          name="description"
-          placeholder="Description"
-          className="textarea-bordered textarea mb-3 w-full"
-        />
-        <input
-          required
-          name="imageUrl"
-          placeholder="Image URL"
-          type="url"
-          className="input-bordered input mb-3 w-full"
-        />
-        <input
-          required
-          name="price"
-          placeholder="Price"
-          type="number"
-          className="input-bordered input mb-3 w-full"
-        />
+  const user = session?.user;
+  let email = user?.email;
+  if (email === "renudilip19@gmail.com") {
+    return (
+      <div>
+        <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+        <form action={addProduct}>
+          <input
+            required
+            name="name"
+            placeholder="Name"
+            className="input-bordered input mb-3 w-full"
+          />
+          <textarea
+            required
+            name="description"
+            placeholder="Description"
+            className="textarea-bordered textarea mb-3 w-full"
+          />
+          <input
+            required
+            name="imageUrl"
+            placeholder="Image URL"
+            type="url"
+            className="input-bordered input mb-3 w-full"
+          />
+          <input
+            required
+            name="price"
+            placeholder="Price"
+            type="number"
+            className="input-bordered input mb-3 w-full"
+          />
 
-        <FormSubmitButton className="btn-block">Add Product</FormSubmitButton>
-      </form>
-    </div>
-  );
+          <FormSubmitButton className="btn-block">Add Product</FormSubmitButton>
+        </form>
+      </div>
+    );
+  } else {
+    return <div> You are not the owner</div>;
+  }
 }
